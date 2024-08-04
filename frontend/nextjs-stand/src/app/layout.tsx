bcb492ab8bc204b66d3b855e1058cf253710fa05
MykhailoYas <maukulwork@gmail.com>
2024-08-04 08:12:44 +0300

    feat(frontend,nextjs): :heavy_plus_sign: Add shadcn UI library

--- frontend/nextjs-stand/src/app/layout.tsx
+++ frontend/nextjs-stand/src/app/layout.tsx
@@ -16,7 +16,12 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+<div className="max-w-screen-xl mx-auto px-4">
+{children}
+</div>
+
+      </body>
     </html>
   );
 }
